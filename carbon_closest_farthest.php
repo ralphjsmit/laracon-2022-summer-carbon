3c0dd7dc2f6fa8788d4e7e87afa519e7e4aab0a9
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-09-14 10:14:09 +0200

    Create carbon_closest_farthest.php

---
+++ carbon_closest_farthest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Carbon;
+
+$dateA = Carbon::parse('today 08:00 am');
+$dateB = Carbon::parse('today 10:00 am');
+
+$baseDate = Carbon::parse('today 09:00 am');
+
+$closestDate = $baseDate->farthest($dateB, $dateA);
+
+dd($closestDate->toDateTimeString());
